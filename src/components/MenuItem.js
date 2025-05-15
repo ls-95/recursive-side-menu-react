6ce9ffd56eaaaa2cc4d3e03555b41492da5271f1
ls-95 <143622355+ls-95@users.noreply.github.com>
2025-05-15 09:51:12 +0200

    Add toggle effect to menu items

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -18,11 +18,16 @@ export default function MenuItem({ item }) {
       <div style={{ display: "flex", gap: "20px" }}>
         <p>{item.label}</p>
         {item && item.children && item.children.length ? (
-          <span onClick={() => handleToggleChildren(item.label)}>+</span>
+          <span onClick={() => handleToggleChildren(item.label)}>
+            {displayCurrentChildren[item.label] ? "-" : "+"}
+          </span>
         ) : null}
       </div>
 
-      {item && item.children && item.children.length > 0 ? (
+      {item &&
+      item.children &&
+      item.children.length > 0 &&
+      displayCurrentChildren[item.label] ? (
         <MenuList list={item.children} />
       ) : null}
     </li>
